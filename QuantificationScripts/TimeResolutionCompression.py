9a3ea3caf4f3ba014fd0f9df43ebb54a2ba96d30
YishaiaZ <yishaiaz@post.bgu.ac.il>
2021-08-08 20:13:50 +0300

    plans (as todo) for calcEndPointProbabilitesOfVaryingCompressionLevels.py amd TimeResolutionCompression.py files).

---
+++ QuantificationScripts/TimeResolutionCompression.py
@@ -0,0 +1,3 @@
+# todo: create a function to compress a single file death data; put a kwarg to either save the
+#  file to the appropriate folder and/or return the compressed XYT coordinates. an additional kwarg should
+#  provide the compressions level required (x2/x3/....).
---
+++ QuantificationScripts/calcEndPointProbabilitesOfVaryingCompressionLevels.py
@@ -0,0 +1,5 @@
+# todo: create a function that calculates using TimeResolutionCompression.py a compression of XYT
+#  coordinates for a given file, then calculates the endpoint nucleators and propagators probabilities.
+#  and returns a dictionary with compression level as key and nucleators and propagators probabilities as values.
+# todo: create a function that plots (in Visualization.py) the results of probabilites about compression levels.
+# todo: perform for all files and select apoptosis and ferroptosis models to show.
