SPI calculation new class implementation New functions and fixes refactored and modified the SPI calculator as SPICalculator class in SPICalculator.py
added skipping treatment types to p_nuc and p_prop calculations by refactoring a function to utils.py.

added a function to calculate and plot the p nucleation of an experiment with a different measurement @ NucleationAndPropagationMeasurements.py

fixed visualize_endpoint_readouts_by_treatment_about_readouts function visualization.py to include legend plotting and added some kwargs.

added correlation function in utils.py with pearson and

QuantificationScripts/NucleationAndPropagationMeasurements.py
@@ -8,6 +8,7 @@ from enum import IntEnum, unique
 from utils import *
 from global_parameters import *
 from Visualization import *
+from SPICalculator import SPICalculator as spiCalc
 
 
 @unique
@@ -449,6 +450,9 @@ def calc_and_visualize_all_experiments_csvs_in_dir(dir_path: str = None,
     """
     only_recent_death_flag_for_neighbors_calc = kwargs.get('only_recent_death_flag_for_neighbors_calc',
                                                            RECENT_DEATH_ONLY_FLAG)
+    treatments_to_include = kwargs.get('treatments_to_include', 'all')
+
+    single_exp_visualize_flag = kwargs.get('visualize_each_exp_flag', False)
 
     visualize_flag = kwargs.get('visualize_flag', True)
 
@@ -488,6 +492,11 @@ def calc_and_visualize_all_experiments_csvs_in_dir(dir_path: str = None,
             exp_file_name=exp_name + '.csv', meta_data_file_full_path=meta_data_file_path,
             compressed_flag=compressed_flag)
 
+        # skip un-wanted treatments
+        if treatments_to_include != 'all' and \
+                verify_any_str_from_lst_in_specific_str(exp_treatment, treatments_to_include):
+            continue
+
         p_nuc_by_time, p_prop_by_time, p_nuc_global, p_prop_global, \
         all_frames_nucleators_mask, all_frames_propagators_mask, \
         accumulated_fraction_of_death_by_time = \
@@ -504,7 +513,7 @@ def calc_and_visualize_all_experiments_csvs_in_dir(dir_path: str = None,
             with open('../experiments_with_bad_results.txt', 'a') as f:
                 f.write(f'exp:{exp_name}| pnuc={p_nuc_global}, pprop={p_prop_global}\n')
 
-        if visualize_flag:
+        if single_exp_visualize_flag:
             visualize_cell_death_in_time(xyt_full_path=file_full_path,
                                          nucleators_mask=all_frames_nucleators_mask,
                                          propagators_maks=all_frames_propagators_mask,
@@ -1225,7 +1234,7 @@ def calc_adjacent_death_variance_in_single_experiment(cells_neighbors: np.array,
                 adjacent_death_variance_in_curr_time.append(dead_cell_neighbors_death_times_variance)
 
             examined_cells_indices.update([curr_dead_cell_idx])
-        if len(adjacent_death_variance_in_curr_time)>0:
+        if len(adjacent_death_variance_in_curr_time) > 0:
             mean_variance_of_adjacent_death_in_curr_time = np.array(adjacent_death_variance_in_curr_time).mean()
         else:
             mean_variance_of_adjacent_death_in_curr_time = 0
@@ -1296,14 +1305,14 @@ def calc_multiple_exps_measurements(main_exp_dir_full_path: str,
                                                            meta_data_file_full_path=meta_data_file_full_path,
                                                            compressed_flag=compressed_flag)
         # skip un-wanted treatments
-        if treatments_to_include != 'all' and sum(
-                [treatment_shortname in exp_treatment.lower() for treatment_shortname in treatments_to_include]) == 0:
+        if treatments_to_include != 'all' and \
+                verify_any_str_from_lst_in_specific_str(exp_treatment, treatments_to_include):
             continue
 
         all_exps_names.append(exp_name)
         all_exps_treatments.append(exp_treatment)
 
-        print(f'analyzing exp {exp_name} | {exp_idx + 1}/{total_exps}')
+        print(f'Calculating {type_of_measurement} for experiment: {exp_name} | Progress: {exp_idx + 1}/{total_exps}')
         exp_df = pd.read_csv(single_exp_full_path)
         cells_xy, cells_times_of_death = exp_df.loc[:, ['cell_x', 'cell_y']].values, exp_df.loc[:,
                                                                                      ['death_time']].values
@@ -1339,6 +1348,14 @@ def calc_multiple_exps_measurements(main_exp_dir_full_path: str,
                 exp_treatment=exp_treatment,
                 **single_exp_kwargs
             )
+        elif type_of_measurement.lower() == 'spi':
+            cells_loci, cells_times_of_death = read_experiment_cell_xy_and_death_times(
+                exp_full_path=single_exp_full_path)
+            single_exp_spi_calculator = spiCalc(XY=cells_loci,
+                                                die_times=cells_times_of_death,
+                                                treatment=exp_treatment,
+                                                temporal_resolution=explicit_temporal_resolution)
+            measurement_endpoint_readout = single_exp_spi_calculator.get_spis()
 
         all_endpoint_readouts_by_experiment.append(measurement_endpoint_readout)
 
@@ -1360,6 +1377,83 @@ def calc_multiple_exps_measurements(main_exp_dir_full_path: str,
                                             dir_to_save_fig_full_path=path_for_fig,
                                             measurement_type=type_of_measurement,
                                             fig_name_suffix=fig_name_suffix)
+
+        return all_endpoint_readouts_by_experiment, all_exps_treatments
+
+
+def calc_pnuc_vs_measurement_for_all_experiments(main_exp_dir_full_path: str,
+                                                 limit_exp_num: int = float('inf'),
+                                                 **kwargs):
+    visualize_flag = kwargs.get('visualize_flag', True)
+    full_dir_path_to_save_fig = kwargs.get('full_dir_path_to_save_fig', None)
+    measurement_type = kwargs.get('type_of_measurement', 'adjacent_death_time_difference')
+
+    # for readability, explicitly provide measurement type
+    kwargs.pop('type_of_measurement')
+
+    # None P(Nuc) measurement calculation
+    all_measurement_endpoint_readouts_by_experiment, all_measurements_exps_treatments = calc_multiple_exps_measurements(
+        main_exp_dir_full_path,
+        limit_exp_num,
+        type_of_measurement=measurement_type,
+        **kwargs
+    )
+    # P Nuc calculation
+    all_global_p_nuc, \
+    all_global_p_prop, \
+    all_p_nuc_treatment_types = calc_and_visualize_all_experiments_csvs_in_dir(
+        dir_path=main_exp_dir_full_path,
+        limit_exp_num=limit_exp_num,
+        **kwargs
+    )
+
+    # SPI calculation
+    all_spi_readouts_by_experiment, all_spi_exps_treatments = calc_multiple_exps_measurements(
+        main_exp_dir_full_path,
+        limit_exp_num,
+        type_of_measurement='spi',
+        **kwargs
+    )
+
+    if (all_p_nuc_treatment_types != all_measurements_exps_treatments).all():
+        raise RuntimeError('calculation of measurement and p nuc are not aligned, sort by exp name!')
+
+    if (all_spi_exps_treatments != all_p_nuc_treatment_types).all():
+        raise RuntimeError('calculation of spi and p nuc are not aligned, sort by exp name!')
+
+
+    spi_and_measurement_correlation = calc_correlation(all_measurement_endpoint_readouts_by_experiment, all_spi_readouts_by_experiment)
+
+    print(f'Correlation between SPI and {measurement_type} is: {spi_and_measurement_correlation}')
+    if visualize_flag:
+        full_dir_path_to_save_fig = os.sep.join(
+            [RESULTS_MAIN_DIR, 'MeasurementsEndpointReadoutsPlots', 'ComparisonBetweenMeasurements']) if full_dir_path_to_save_fig is None else full_dir_path_to_save_fig
+
+        visualize_endpoint_readouts_by_treatment_about_readouts(x_readout=all_global_p_nuc,
+                                                                y_readout=all_measurement_endpoint_readouts_by_experiment,
+                                                                treatment_per_readout=all_p_nuc_treatment_types,
+                                                                full_dir_path_to_save_fig=full_dir_path_to_save_fig,
+                                                                x_label=f'Fraction of Nucleators',
+                                                                y_label=measurement_type,
+                                                                fig_title=f'Fraction of Nucleators about\n {measurement_type}',
+                                                                use_log=False,
+                                                                set_y_lim=False,
+                                                                show_legend=True,
+                                                                fig_name_to_save=f'fraction_of_nuc_about_{measurement_type}',
+                                                                **kwargs)
+
+        visualize_endpoint_readouts_by_treatment_about_readouts(x_readout=all_global_p_nuc,
+                                                                y_readout=all_spi_readouts_by_experiment,
+                                                                treatment_per_readout=all_p_nuc_treatment_types,
+                                                                full_dir_path_to_save_fig=full_dir_path_to_save_fig,
+                                                                x_label='Fraction of Nucleators',
+                                                                y_label='SPI',
+                                                                fig_title=f'Fraction of Nucleators about\n {"SPI"}',
+                                                                use_log=False,
+                                                                set_y_lim=False,
+                                                                show_legend=True,
+                                                                fig_name_to_save=f'fraction_of_nuc_about_{"SPI"}',
+                                                                **kwargs)
 
 
 if __name__ == '__main__':
@@ -1481,12 +1575,23 @@ if __name__ == '__main__':
     #                                 )
 
     # time of death variance between adjacent cells in an entire experiment
+    # dir_path = os.sep.join(os.getcwd().split(os.sep)[:-1] + ['Data', 'Experiments_XYT_CSV', 'OriginalTimeMinutesData'])
+    # calc_multiple_exps_measurements(main_exp_dir_full_path=dir_path,
+    #                                 limit_exp_num=float('inf'),
+    #                                 use_sliding_time_window=False,
+    #                                 type_of_measurement='adjacent_death_time_variance',
+    #                                 treatments_to_include=['superkiller', 'fac', 'erastin', 'simulation'],
+    #                                 visualize_flag=True,
+    #                                 visualize_each_exp_flag=False)
+
+    # p_nuc about adjacent death time difference measurement
     dir_path = os.sep.join(os.getcwd().split(os.sep)[:-1] + ['Data', 'Experiments_XYT_CSV', 'OriginalTimeMinutesData'])
-    calc_multiple_exps_measurements(main_exp_dir_full_path=dir_path,
-                                    limit_exp_num=float('inf'),
-                                    use_sliding_time_window=False,
-                                    type_of_measurement='adjacent_death_time_variance',
-                                    treatments_to_include=['superkiller', 'fac', 'erastin', 'simulation'],
-                                    visualize_flag=True,
-                                    visualize_each_exp_flag=False
-                                    )
+    calc_pnuc_vs_measurement_for_all_experiments(main_exp_dir_full_path=dir_path,
+                                                 limit_exp_num=float('inf'),
+                                                 use_sliding_time_window=False,
+                                                 type_of_measurement='adjacent_death_time_difference',
+                                                 bins_of_adjacent_death_diff=np.arange(0, 4, 0.5),
+                                                 treatments_to_include=['superkiller', 'dots', 'h2O2', 'erastin'],
+                                                 visualize_flag=True,
+                                                 visualize_each_exp_flag=False
+                                                 )

Visualization.py
@@ -173,11 +173,17 @@ def visualize_endpoint_readouts_by_treatment_about_readouts(x_readout: np.array,
                                                             full_path_to_save_fig: str = None,
                                                             x_label: str = 'Fraction of Nucleators',
                                                             y_label: str = 'Fraction of Propagators',
+                                                            fig_title: str = 'Fraction of Nucleators & Propagators',
+                                                            fig_name_to_save: str = None,
                                                             use_log: bool = False,
                                                             plot_about_treatment: bool = False,
                                                             **kwargs):
     set_y_lim = kwargs.get('set_y_lim', True)
+    show_legend = kwargs.get('show_legend', False)
+    legend_font_size = kwargs.get('legend_font_size', 6)
+    legend_marker_ration = kwargs.get('legend_marker_ration', 0.8)
     recent_death_flag = kwargs.get('only_recent_death_flag_for_neighbors_calc', RECENT_DEATH_ONLY_FLAG)
+
     if use_log:
         x_readout = np.log10(x_readout)
         y_readout = np.log10(y_readout)
@@ -208,9 +214,6 @@ def visualize_endpoint_readouts_by_treatment_about_readouts(x_readout: np.array,
         else:
             ax.plot(x, y, ms=marker_size, marker=marker,
                     color=color, label=treatment_per_readout[point_idx])
-
-    custom_handles, custom_lables = get_custom_legend_artists(labels_to_colors=treatment_to_color,
-                                                              labels_to_markers=treatment_to_marker)
 
     if plot_about_treatment:
         # temporal_unit = 'Minutes' if temporal_resolution != 1 else 'Frame#'
@@ -262,7 +265,7 @@ def visualize_endpoint_readouts_by_treatment_about_readouts(x_readout: np.array,
         ax.grid('on')
         ax.set_xlabel('Log ' + x_label if use_log else x_label)
         ax.set_ylabel('Log ' + y_label if use_log else y_label)
-        ax.set_title('Log(Fraction) of Nucleators & Propagators' if use_log else 'Fraction of Nucleators & Propagators')
+        ax.set_title(fig_title if not use_log else f'Log of {fig_title}')
         if not use_log:
             x_tick_limits = [0.1, .30]
             y_tick_limits = [0.6, 0.95]
@@ -272,6 +275,24 @@ def visualize_endpoint_readouts_by_treatment_about_readouts(x_readout: np.array,
         if set_y_lim:
             ax.set_xlim(x_tick_limits)
             ax.set_ylim(y_tick_limits)
+
+    if show_legend:
+        # filter treatments colors and markers to show only computed treatments in legend
+        filtered_treatment_to_color = {}
+        filtered_treatment_to_marker = {}
+        for key, val in treatment_to_color.items():
+            if key in treatment_per_readout:
+                filtered_treatment_to_color[key] = val
+                filtered_treatment_to_marker[key] = treatment_to_marker[key]
+
+        custom_handles, custom_lables = get_custom_legend_artists(labels_to_colors=filtered_treatment_to_color,
+                                                                  labels_to_markers=filtered_treatment_to_marker)
+
+        plt.legend(handles=custom_handles,
+                   labels=custom_lables,
+                   loc='best',
+                   fontsize=legend_font_size,
+                   markerscale=legend_marker_ration)
 
     plt.tight_layout()
 
@@ -291,11 +312,15 @@ def visualize_endpoint_readouts_by_treatment_about_readouts(x_readout: np.array,
         if not os.path.isdir(path_for_plot_dir):
             os.makedirs(path_for_plot_dir)
 
-        plot_about_time_path_addition = '_about_treatment' if plot_about_treatment else ''
-        if use_log:
-            path_for_plot = os.sep.join([path_for_plot_dir, f'LOG_global_p_nuc_vs_p_prop_all_exps{plot_about_time_path_addition}'])
+        if fig_name_to_save is None:
+            plot_about_time_path_addition = '_about_treatment' if plot_about_treatment else ''
+            if use_log:
+                path_for_plot = os.sep.join([path_for_plot_dir, f'LOG_global_p_nuc_vs_p_prop_all_exps{plot_about_time_path_addition}'])
+            else:
+                path_for_plot = os.sep.join([path_for_plot_dir, f'global_p_nuc_vs_p_prop_all_exps{plot_about_time_path_addition}'])
         else:
-            path_for_plot = os.sep.join([path_for_plot_dir, f'global_p_nuc_vs_p_prop_all_exps{plot_about_time_path_addition}'])
+            path_for_plot = os.sep.join([path_for_plot_dir, fig_name_to_save])
+
         path_for_plot = path_for_plot if full_path_to_save_fig is None else full_path_to_save_fig
         plt.savefig(path_for_plot + '.png', dpi=200)  # , bbox_extra_artists=[lgd], bbox_inches='tight')
         plt.savefig(path_for_plot + '.eps', dpi=200)  # , bbox_extra_artists=[lgd], bbox_inches='tight')

utils.py
@@ -3,7 +3,8 @@ from typing import *
 import numpy as np
 import pandas as pd
 from scipy.spatial import Voronoi
-from scipy.stats import linregress, zscore
+from scipy.stats import linregress, zscore, pearsonr
+from scipy.signal import correlate
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics.pairwise import euclidean_distances as euc_dis
@@ -277,7 +278,7 @@ def get_linear_regression_line_between_two_signals(x: np.array, y: np.array) -> 
     return x_new, y_new
 
 
-def get_cells_neighbors(XY, threshold_dist: Union[int, float] = None) ->Tuple[List[int], List[int], List[int]]:
+def get_cells_neighbors(XY, threshold_dist: Union[int, float] = None) -> Tuple[List[int], List[int], List[int]]:
     """
     returns 3 levels of topological neighbors for each cell.
     if threshold_dist is not None, a distance constraint is employed on the neighbors to prevent neighbors that are
@@ -455,7 +456,7 @@ def clean_string_from_bad_chars(treatment_name: str, replacement='_') -> str:
     return treatment_name.replace('\\', replacement).replace('/', replacement)
 
 
-def normalize(values: np.array, normalization_method:str = 'z_score', axis: int = 0):
+def normalize(values: np.array, normalization_method: str = 'z_score', axis: int = 0):
     if normalization_method == 'z_score':
         return zscore(values, axis=axis)
     Warning('the normalization method is unknown!')
@@ -479,3 +480,43 @@ def get_dead_cells_mask_in_window(window_start_time_min: int,
         return (cells_times_of_death >= window_start_time_min) * (cells_times_of_death < window_end_time_min)
     else:
         return cells_times_of_death < window_end_time_min
+
+
+def verify_any_str_from_lst_in_specific_str(str_to_verify: str, lst_of_strings: List[str]) -> bool:
+    """
+    checks if any string from a list of strings appears in another string.
+    Used to check whether any of a list of shortened/lazy versions of treatments' names appear in a treatment full name.
+    The purpose of this function is to skip un-wanted treatments.
+    :param str_to_verify: str, the full string we we like to find instances of at least one of the strings in lst_of_strings
+    :param lst_of_strings: List[str], a list of shortened versions of strings.
+    :return: boolean
+    """
+    return sum(
+        [treatment_shortname.lower() in str_to_verify.lower() for treatment_shortname in lst_of_strings]) == 0
+
+
+def calc_correlation(x: np.array, y: np.array, type_of_correlation: str = None,
+                     print_p_val: bool = True, **kwargs) -> Union[float, np.array]:
+    """
+    default is pearson correlation (if type_of_correlation argument is None)
+    :param x: np.array
+    :param y: np.array
+    :param type_of_correlation: str
+    :param print_p_val: bool
+    :param kwargs: any kwargs for correlation functions of scipy
+    :return:
+    """
+    p_val = None
+
+    if type_of_correlation == 'discrete_correlation':
+        correlation = correlate(x, y,
+                                mode=kwargs.get('correlate_mode', 'full'),
+                                method=kwargs.get('correlate_method', 'auto'))
+    else:
+        Warning('Pearson correlation assumes normal distribution of x and y')
+        correlation, p_val = pearsonr(x, y)
+
+    if print_p_val:
+        print(f'p value of {type_of_correlation}={p_val}')
+
+    return correlation