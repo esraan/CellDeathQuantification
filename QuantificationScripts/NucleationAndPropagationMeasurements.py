SPI calculation new class implementation New functions and fixes refactored and modified the SPI calculator as SPICalculator class in SPICalculator.py
added skipping treatment types to p_nuc and p_prop calculations by refactoring a function to utils.py.

added a function to calculate and plot the p nucleation of an experiment with a different measurement @ NucleationAndPropagationMeasurements.py

fixed visualize_endpoint_readouts_by_treatment_about_readouts function visualization.py to include legend plotting and added some kwargs.

added correlation function in utils.py with pearson and

QuantificationScripts/NucleationAndPropagationMeasurements.py
@@ -1591,7 +1591,7 @@ if __name__ == '__main__':
                                                  use_sliding_time_window=False,
                                                  type_of_measurement='adjacent_death_time_difference',
                                                  bins_of_adjacent_death_diff=np.arange(0, 4, 0.5),
-                                                 treatments_to_include=['fac', 'superkiller', 'dots', 'h2O2', 'erastin'],
+                                                 treatments_to_include=['fac&bso'],#['fac&bso', 'h2O2'], #['superkiller', 'dots', 'h2O2', 'erastin'],
                                                  visualize_flag=True,
                                                  visualize_each_exp_flag=False
                                                  )